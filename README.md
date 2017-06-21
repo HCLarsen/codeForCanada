15db4e1f032aa68b4673935acc4d9f948e6f7893
Chris Larsen <clarsenipod@gmail.com>
2017-06-21 15:31:01 -0400

    Final Submission

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Language: Ruby
 ## Author: Chris Larsen
 
-This command line tool parses a CSV of building code violations, and presents the information in two ways. First, it gives the number of violations in each category. Second, the first and last violations, by date.
+This command line tool parses a CSV of building code violations, and presents the information in two ways. First, it gives the number of violations in each category. Second, the first and last violations of each category, by date.
 
 ## Usage
 
--- codeForCanada.rb
+++ codeForCanada.rb
@@ -2,5 +2,27 @@ require 'csv'
 
 violations = CSV.read("dev-challenge-data.csv")
 
-puts "Earliest violation is #{earliest}."
-puts "Latest violation is #{latest}"
+violationsByCategory = Hash.new(0)
+
+violations.each do |violation|
+  violationsByCategory[violation[2]] += 1
+end
+
+firstAndLastByCategory = Hash.new([])
+violations.each do |violation|
+end
+
+puts
+puts "Building Code Violation Analysis"
+puts
+puts "Number of Violations in Each Category"
+puts "-------------------------------------"
+puts "Category\tNumber of Violations"
+violationsByCategory.each do |category, violations|
+  puts "#{category}\t#{violations}"
+end
+
+puts
+puts "First and Last Violation Per Category"
+puts "-------------------------------------"
+puts "Category\tFirst Violation\tSecond Violation"
